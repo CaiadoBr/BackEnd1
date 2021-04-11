8aef761f67cfcc990ef9e74c93d03a2d69fc92d9
CaiadoBr <caio.silvestre@academico.ifpb.edu.br>
2021-04-11 17:45:24 -0300

    Banco de dados

---
+++ src/Model/organizador.js
@@ -0,0 +1,16 @@
+const {Sequelize, DataTypes} = require("sequelize");
+const db = require("../db");
+
+const Organizador = db.define("Organizador",
+    {
+        Id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            references: {
+                model: Organizador, 
+                key: 'id'
+            }
+        },
+    
+    }
+);
--- src/bd.js
+++ src/bd.js
@@ -1,7 +1,7 @@
 const {Pool} = require("pg")
 async function criarConexao() {
     const pool = new Pool({
-        connectionString: 'postgres://yprezhfexzmsst:bd058ca4df1b18b5fe6e81bbf84ff913abbbc150a9b6b6d2e7745a753b122c64@ec2-52-1-115-6.compute-1.amazonaws.com:5432/d9prot76bf3ps9', 
+        connectionString: 'postgres://fxyozawexqxxmn:b5cfeedbb8fe4b91c403c62f8e8a94fabb20eaedc4f9aaf8e0de3ca6713d692c@ec2-35-174-35-242.compute-1.amazonaws.com:5432/dletfdn37jdpd', 
         ssl: {
             rejectUnauthorized: false
         }
